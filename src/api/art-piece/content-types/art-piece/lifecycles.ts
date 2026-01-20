26ec698905aedeb89d60ca65e69293fc6548a18d
Hakon Bjarnason <hakonbjarnason@sijthoffmedia.nl>
2026-01-20 21:33:52 +0100

    Remove unsupported lifecycle hooks

--- src/api/art-piece/content-types/art-piece/lifecycles.ts
+++ src/api/art-piece/content-types/art-piece/lifecycles.ts
@@ -10,10 +10,4 @@ export default {
   async afterDelete() {
     await triggerFrontendRebuild();
   },
-  async afterPublish() {
-    await triggerFrontendRebuild();
-  },
-  async afterUnpublish() {
-    await triggerFrontendRebuild();
-  },
 };
